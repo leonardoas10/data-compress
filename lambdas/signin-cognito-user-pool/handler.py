f8b6f00bf075bf758e7b617b199701fdc8391673
Leonardo Aranguren S <leoaranguren10@gmail.com>
2022-11-13 01:09:33 -0300

    Adding: signin lambda

--- lambdas/signin-cognito-user-pool/handler.py
+++ lambdas/signin-cognito-user-pool/handler.py
@@ -1,8 +1,32 @@
 import boto3
+import os
+import json
+
+client = boto3.client('cognito-idp')
 
 def handler(event, context):
     try:
-        print(event, context)
+        data = json.loads(event['body'])
+        username = data['email']
+        password = data['password']
+        print(username, password)
+        response = client.admin_initiate_auth(
+            UserPoolId=os.getenv('COGNITO_POOL_ID'),
+            ClientId=os.getenv('CLIENT_ID'),
+            AuthParameters={
+                'USERNAME': username,
+                'PASSWORD': password,
+            },
+        )
+        return {
+            'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
+            'headers': {
+                'Access-Control-Allow-Origin': '*'
+            },
+              'body': json.dumps({
+                    'data': response,   
+                }, sort_keys=True)
+        }
     except Exception as e:
         print('Error handler() => ', e)
         return e
