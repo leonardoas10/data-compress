f4db4ce8294e1c2df03631506ffa4487be6352a4
Leonardo Aranguren S <leoaranguren10@gmail.com>
2022-11-13 21:50:20 -0300

    Adding: signin logic for obtain access and secret key

--- lambdas/signin-cognito-user-pool/handler.py
+++ lambdas/signin-cognito-user-pool/handler.py
@@ -3,28 +3,55 @@ import os
 import json
 
 client = boto3.client('cognito-idp')
+identity = boto3.client('cognito-identity')
+
+logins = 'cognito-idp.{}.amazonaws.com/{}'.format(os.getenv('AWS_REGION'), os.getenv('COGNITO_POOL_ID'))
 
 def handler(event, context):
     try:
         data = json.loads(event['body'])
         username = data['email']
         password = data['password']
-        print(username, password)
+
         response = client.admin_initiate_auth(
             UserPoolId=os.getenv('COGNITO_POOL_ID'),
             ClientId=os.getenv('CLIENT_ID'),
+            AuthFlow='ADMIN_USER_PASSWORD_AUTH',
             AuthParameters={
                 'USERNAME': username,
                 'PASSWORD': password,
             },
         )
+        
+        print(response)
+        
+        id_token = response['AuthenticationResult']['IdToken']
+        
+        identityId = identity.get_id(
+            IdentityPoolId=os.getenv('IDENTITY_POOL_ID'),
+            Logins={
+                logins: id_token
+            }
+        )['IdentityId']
+        
+        aws_cred = identity.get_credentials_for_identity(
+            IdentityId=identityId,
+            Logins={
+                logins: id_token
+            }
+        )['Credentials']
+        
+        print(aws_cred)
+        
         return {
             'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
             'headers': {
                 'Access-Control-Allow-Origin': '*'
             },
               'body': json.dumps({
-                    'data': response,   
+                    'token': id_token,
+                    'accessKeyId': aws_cred['AccessKeyId'],
+                    'secretKey': aws_cred['SecretKey']
                 }, sort_keys=True)
         }
     except Exception as e:
